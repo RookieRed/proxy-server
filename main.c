7b5d06ac8fc6a6713c8d24fbf2fd0ebcf82770ba
fauvet <g.fauvet.perso@gmail.com>
2015-12-08 12:07:54 +0100

    [SUPPRESSION] Commentaires 'parasites'

--- main.c
+++ main.c
@@ -6,19 +6,6 @@
 **              PROJET PROXY SERVER           **
 ************************************************/
 
-
-//Inclusion des bibliothèques
-
-//Inclusion des headers du projet
-
-//Déclaration des constantes globales
-
-
-//Fonction main
 int main(){
-
-	//Déclaration des variables locales
-
-
-	//...
+	
 }
