3cc00f92c0d0d35e7dec810b64cdd8d2d8a41cb2
rookiered <vous@exemple.com>
2015-12-08 12:02:51 +0100

    Ajout de la doc (client serveur corrigés par Laborde)

--- exemple.c
+++
@@ -1,5 +0,0 @@
-#include "include/exemple.h"
-
-int main() {
-
-}
---
+++ main.c
@@ -0,0 +1,24 @@
+/***********************************************
+** FICHIER MAIN.C POINT D'ENTRÉE DU PROGRAMME **
+************************************************
+**     Eloundou Cédric - Fauvet Guillaume     **
+**                                            **
+**              PROJET PROXY SERVER           **
+************************************************/
+
+
+//Inclusion des bibliothèques
+
+//Inclusion des headers du projet
+
+//Déclaration des constantes globales
+
+
+//Fonction main
+int main(){
+
+	//Déclaration des variables locales
+
+
+	//...
+}
