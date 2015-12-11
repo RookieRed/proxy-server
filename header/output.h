4bfefe815d1e64f3dce7435ae420ee803e9fc065
rookiered <vous@exemple.com>
2015-12-11 10:17:08 +0100

    ok

---
+++ header/output.h
@@ -0,0 +1,19 @@
+/***********************************************
+**              FICHIER OUTPUT.H              **
+************************************************
+**     Eloundou Cédric - Fauvet Guillaume     **
+**                                            **
+**              PROJET PROXY SERVER           **
+************************************************/
+
+/**
+* Ecrit à l'écran un message d'erreur avec son code
+* @param message : le message à afficher
+*/
+void ecrireErreur(const char * message);
+
+/**
+* Ecrit à l'écran un message d'information
+* @param message : le message à afficher
+*/
+void ecrireInfo(const char * message);
